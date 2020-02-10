9cd388cc6f3890dab5a4915ddbbee843485a37a1
Svitlana Grytsai <34807666+SvitlanaG@users.noreply.github.com>
2020-02-10 20:17:57 +0100

    CRUD in progress

--- VeganRecipesProject/vegan-recipes-rating-data-service/src/main/java/distrsystems/htwproject/veganrecipesratingdataservice/controllers/RatingController.java
+++ VeganRecipesProject/vegan-recipes-rating-data-service/src/main/java/distrsystems/htwproject/veganrecipesratingdataservice/controllers/RatingController.java
@@ -5,6 +5,7 @@ import distrsystems.htwproject.veganrecipesratingdataservice.converters.RatingTo
 import distrsystems.htwproject.veganrecipesratingdataservice.domain.Rating;
 import distrsystems.htwproject.veganrecipesratingdataservice.services.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class RatingController {
@@ -37,15 +39,21 @@ public class RatingController {
     }
 
     @RequestMapping({"/rating/list", "/rating"})
-    public String listRatings(Model model){
-        model.addAttribute("ratings", ratingService.listAll());
-        return "product/list";
+    public ResponseEntity<List> listRatings(Model model){
+        List<Rating> ratings = ratingService.listAll();
+        //model.addAttribute("ratings", ratings);
+        return ResponseEntity.ok(ratings);
     }
 
     @RequestMapping("/rating/show/{id}")
-    public String getRating(@PathVariable String id, Model model){
-        model.addAttribute("rating", ratingService.getById(Long.valueOf(id)));
-        return "product/show";
+    public ResponseEntity<Rating> getRating(@PathVariable String id, Model model){
+        Rating rating = ratingService.getById(Long.valueOf(id));
+
+        if (rating == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(rating);
+        }
     }
 
     @RequestMapping("rating/edit/{id}")
